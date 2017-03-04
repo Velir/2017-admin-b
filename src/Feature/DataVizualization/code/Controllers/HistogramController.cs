028bab83552adeaf5aa43fd2a0daf81d7ba75bec
dsolovay <dan.solovay@velir.com>
2017-03-04 06:13:41 -0500

    Use SXA controller

--- src/Feature/DataVizualization/code/Controllers/HistogramController.cs
+++ src/Feature/DataVizualization/code/Controllers/HistogramController.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Sitecore.XA.Foundation.Mvc.Controllers;
 
 namespace AdminB2017.Feature.DataVisualization.Controllers
 {
-    public class HistogramController : Controller
+    public class HistogramController : StandardController
     {
         // GET: Histogram
-        public ActionResult Index()
-        {
-          return View("~/Views/DataVisualization/Histogram.cshtml");
-        }
+        //public ActionResult Index()
+        //{
+        //  return View("~/Views/DataVisualization/Histogram.cshtml");
+        //}
     }
 }
