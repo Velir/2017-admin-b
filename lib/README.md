e1226191ee9c669011faa9a2c17acfb32eebdc12
dsolovay <dan.solovay@velir.com>
2017-03-04 03:30:00 -0500

    Make Controller rendering, add SXA dlls

---
+++ lib/README.md
@@ -0,0 +1,9 @@
+# SXA Dependencies
+
+Since SXA DLLs are not available yet on Sitecore's NuGet feed, it is necessary to add them here.
+
+The following are required:  
+
+* Sitecore.XA.Foundation.MVC.dll
+* Sitecore.XA.Foundation.IOC.dll
+
---
+++ src/Feature/DataVizualization/code/Controllers/HistogramController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdminB2017.Feature.DataVisualization.Controllers
+{
+    public class HistogramController : Controller
+    {
+        // GET: Histogram
+        public ActionResult Index()
+        {
+          return View("~/Views/DataVisualization/Histogram.cshtml");
+        }
+    }
+}
