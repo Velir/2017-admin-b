2477c3ce5edda7798056314b9cbfb608e7da80e8
Nicole DuRand <nicole.durand@velir.com>
2017-03-04 16:25:17 -0500

    Error handling for histogram!

--- src/Feature/DataVizualization/code/Models/HistogramRenderingModel.cs
+++ src/Feature/DataVizualization/code/Models/HistogramRenderingModel.cs
@@ -11,9 +11,17 @@ namespace AdminB2017.Feature.DataVisualization.Models
   public class HistogramRenderingModel : RenderingModelBase
   {
     private readonly string _id;
-    public HistogramRenderingModel()
+        private readonly DateTime _fromDate = DateTime.Now.AddMonths(-1);
+        private readonly DateTime _toDate = DateTime.Now;
+        private readonly TimePeriods _timePeriod = TimePeriods.Day;
+        private readonly bool _showLabels = true;
+        public HistogramRenderingModel()
     {
       _id = "a" + Guid.NewGuid().ToString("N");
+            FromDate = _fromDate;
+            ToDate = _toDate;
+            TimePeriod = _timePeriod;
+            ShowLabels = _showLabels;
     }
 
     /// <summary>
@@ -33,8 +41,8 @@ namespace AdminB2017.Feature.DataVisualization.Models
 
     public string DateColumnName { get; set; }
 
-        public string MessageRequiredFieldMissing {  get { return "Your datasource is missing some required information.  You must supply a data link to a valid csv and the dat column name."; } }
-  }
+        public string MessageRequiredFieldMissing {  get { return "Your datasource is missing some required information.  You must supply a data link to a valid csv and the data column name."; } }
+    }
 
  
 }
--- src/Feature/DataVizualization/code/Repositories/HistogramRepository.cs
+++ src/Feature/DataVizualization/code/Repositories/HistogramRepository.cs
@@ -13,23 +13,30 @@ namespace AdminB2017.Feature.DataVisualization.Repositories
 {
   public class HistogramRepository : ModelRepository, IHistogramRepository, IModelRepository, IAbstractRepository<IRenderingModelBase>
   {
-    public override IRenderingModelBase GetModel()
-    {
-      HistogramRenderingModel model = new HistogramRenderingModel();
-      this.FillBaseProperties(model);
-      //TODO Handle null datasource.
-      LookupField timePeriod = model.DataSourceItem.Fields[Templates.Histogram.Fields.TimePeriod];
-      model.TimePeriod = (TimePeriods)Enum.Parse(typeof(TimePeriods), timePeriod.TargetItem.Fields["Value"].Value); //TODO Pull from Foundation.  And put this in a method.
-      LinkField field = model.DataSourceItem.Fields[Templates.Histogram.Fields.Data];
-      model.DataUrl = new Uri(field.Url);
-      model.ShowLabels =  !(string.IsNullOrWhiteSpace(this.Rendering.Parameters[Constants.ShowLabels]) ||
-      this.Rendering.Parameters[Constants.ShowLabels] == "0"); //TODO Clean up
-      DateField fromDate = model.DataSourceItem.Fields[Templates.Histogram.Fields.FromDate];
-      model.FromDate = fromDate.DateTime;
-            DateField toDate = model.DataSourceItem.Fields[Templates.Histogram.Fields.ToDate];
-            model.ToDate = toDate.DateTime;
-
-            model.DateColumnName = model.DataSourceItem.Fields[Templates.Histogram.Fields.DataColumnName].Value;
+        public override IRenderingModelBase GetModel()
+        {
+            HistogramRenderingModel model = new HistogramRenderingModel();
+            this.FillBaseProperties(model);
+            if (model.DataSourceItem != null)
+            {
+                LookupField timePeriod = model.DataSourceItem.Fields[Templates.Histogram.Fields.TimePeriod];
+                if (timePeriod.TargetItem != null)
+                    model.TimePeriod = (TimePeriods)Enum.Parse(typeof(TimePeriods), timePeriod.TargetItem.Fields["Value"].Value); //TODO Pull from Foundation.  And put this in a method.
+                LinkField field = model.DataSourceItem.Fields[Templates.Histogram.Fields.Data];
+                if (!string.IsNullOrWhiteSpace(field.Url))
+                    model.DataUrl = new Uri(field.Url);
+                model.ShowLabels = !(string.IsNullOrWhiteSpace(this.Rendering.Parameters[Constants.ShowLabels]) ||
+                this.Rendering.Parameters[Constants.ShowLabels] == "0"); //TODO Clean up
+                DateField fromDate = model.DataSourceItem.Fields[Templates.Histogram.Fields.FromDate];
+                if (fromDate.DateTime != DateTime.MinValue)
+                    model.FromDate = fromDate.DateTime;
+                DateField toDate = model.DataSourceItem.Fields[Templates.Histogram.Fields.ToDate];
+                if (toDate.DateTime != DateTime.MinValue)
+                    model.ToDate = toDate.DateTime;
+                TextField dataColumnName = model.DataSourceItem.Fields[Templates.Histogram.Fields.DataColumnName];
+                if (dataColumnName.Value != null)
+                    model.DateColumnName = dataColumnName.Value;
+            }
       return model;
     }
   }
